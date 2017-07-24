b99490b7ce9fefc837af3baa9a78bdfe27430cb2
Marcel Schwittlick <lzrm@mrzl.net>
2017-07-24 22:46:29 +0200

    includes of dependency

--- src/applications/test1/main.cpp
+++ src/applications/test1/main.cpp
@@ -4,8 +4,33 @@
 
 #include <iostream>
 
+#include "ofMain.h"
+#include "../../modules/path/pathmanager.h"
+
+class ofApp : public ofBaseApp {
+public:
+
+    int paths_loaded;
+
+    void setup() {
+        pathmanager m;
+        paths_loaded = m.load("/home/mar/code/composition/data/testing/");
+        std::cout << paths_loaded << std::endl;
+    }
+
+    void update() {
+    }
+
+    void draw() {
+        ofBackground( 0 );
+        ofSetColor(255),
+        ofDrawBitmapString(ofToString(paths_loaded), ofGetMouseX(), ofGetMouseY());
+    }
+};
+
 int main()
 {
-    std::cout << "workin" << std::endl;
+    ofSetupOpenGL(1024,768, OF_WINDOW);
+    ofRunApp( new ofApp());
     return 0;
 }
